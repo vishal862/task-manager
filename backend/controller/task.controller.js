78422ef10293e325fc88227134f26053c53a1eef
“Vishal <vishaldevkate1981@gmail.com>
2025-07-01 15:24:12 +0530

    get tasks route

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -1,18 +1,37 @@
 import Task from "../model/task.model.js";
 
-export const test = (req,res) =>{
-    return res.json({message : "running"})
-}
+export const test = (req, res) => {
+  return res.json({ message: "running" });
+}; 
+
 export const createTask = async (req, res) => {
   try {
-    const {title, description, assignedTo} = req.body;
-    if(!title || !description || !assignedTo){
-        return res.status(404).json({message : "all fields are required"})
+    const { title, description, assignedTo } = req.body;
+    if (!title || !description || !assignedTo) {
+      return res.status(404).json({ message: "all fields are required" });
     }
     const task = await Task.create(req.body);
     res.status(201).json(task);
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: error.message });
+  }
+};
+
+export const getTasks = async (req, res) => {
+  const { status, assignedTo } = req.query;
+  const query = {};
+
+  if (status) query.status = status;
+  if (assignedTo) query.assignedTo = assignedTo;
+    console.log(query);
+    
+  try {
+    const tasks = await Task.find(query);
+    console.log(tasks);
+    
+    return res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
