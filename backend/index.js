2634e806e86e154fab255e737ca316371e62c2ec
“Vishal <vishaldevkate1981@gmail.com>
2025-07-01 14:33:08 +0530

    db setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server running")))
+  .catch((err) => console.error(err));
