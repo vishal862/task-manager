4fbd88fd1343ad3023e0bcb13d5538fda9e01220
“Vishal <vishaldevkate1981@gmail.com>
2025-07-01 18:37:20 +0530

    installation for render

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import taskRoute from "./routes/task.route.js"
 
+
+const __dirname = path.resolve();
+
+
 dotenv.config();
 const app = express();
 
@@ -11,6 +15,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/tasks",taskRoute)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
 
 mongoose
   .connect(process.env.MONGO_URI)
