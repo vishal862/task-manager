4f873ad14fcf26c39fe1a099779ccc7c95c8f8c9
“Vishal <vishaldevkate1981@gmail.com>
2025-07-01 19:13:20 +0530

    import change

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,9 @@ const path = require("path");
 
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const path = require("path");
+const __dirname = path.resolve();
+
 
 // Serve static frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
