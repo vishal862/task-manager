6c9643bc676b9da409b8e62d29aff75f5bc4e962
“Vishal <vishaldevkate1981@gmail.com>
2025-07-01 19:17:20 +0530

    redeclaration removed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const taskRoute = require("./routes/task.route");
-const path = require("path");
 
 
 
