bbb1f3ca8260bb8c9efb3504cd2733f18395f4a6
“Vishal <vishaldevkate1981@gmail.com>
2025-07-01 15:24:12 +0530

    get tasks route

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createTask, test } from "../controller/task.controller.js";
+import { createTask, getTasks, test } from "../controller/task.controller.js";
 
 const router = express.Router();
 
-router.get("/test", test)
+router.get("/test", test);
 router.post("/", createTask);
+router.get("/", getTasks);
 
-export default router
+export default router;
